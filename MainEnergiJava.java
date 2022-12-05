f24b53b18ebc7a9b899c988728f7fb91a100a34e
Ivanwisnu32 <118862185+Ivanwisnu32@users.noreply.github.com>
2022-12-05 12:22:11 +0700

    Add files via upload

---
+++ MainEnergiJava.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package main.energijava;
+
+/**
+ *
+ * @author WINDOWS 8
+ */
+public class MainEnergiJava {
+
+    
+  
+    public static void main(String[] args) {
+       MainEnergi me = new MainEnergi();
+       Scanner input = new Scanner(source:System.in)
+       
+       System.out.print(s: " Masukkan Kecepatan benda = ");
+       Double v = input.nextDouble();
+       me.setKecepatanBenda(kecepatanbenda : V);
+       System.out.println();
+       
+       System.out.print(s:" Masukkan massa benda");
+       Double m = input.nextDouble();
+       me.setKecepatanBenda(massabenda:m);
+       System.out.println();
+       
+       Double ek = 0.5/me.getMassabenda()* me.kecepatanBenda*me
+       me.setEk(Ek:ek);
+       
+       System .ot.println("jadi Hasilnya adalah " + me.getEk() + ")
+       
+    
+    
+    
+}
